2667adaf22a83445e3e9b8d2b4c1534a5a7421fb
Joy Phoebe <38925103+joyapisi@users.noreply.github.com>
2023-03-17 00:04:49 +0300

    add mobile menu pop up and link all menu items to the appropriate pages

--- mobile_menu.js
+++ mobile_menu.js
@@ -1,16 +1,36 @@
-const NavList = document.getElementById('navigationList');
-const Hamburger = document.getElementById('hamburger');
-const CloseButton = document.getElementById('closeButton');
+// function menuPopup() {
+//   const mobileMenu = document.getElementById("navList");
+//   const CloseButton = document.getElementById('closeButton');
+//   if (mobileMenu.style.display === "block") {
+//     mobileMenu.style.display = "none";
+//   } else {
+//     mobileMenu.style.display = "block";
+//   }
+//   if (CloseButton.style.display === "block") {
+//     CloseButton.style.display = "none";
+//   } else {
+//     CloseButton.style.display = "block";
+//   }
+// }
+
+
+
+
 
 function menuPopup() {
+  const NavList = document.getElementById('navList');
+  const Hamburger = document.getElementById('hamburger');
+  const CloseButton = document.getElementById('closeButton');
+
   NavList.classList.toggle('active');
   Hamburger.classList.toggle('active');
   CloseButton.classList.toggle('active');
 
-//   if (window.innerWidth > 768) {
-//     document.getElementById('navigationList').classList.toggle('active');
-//     return false;
-//   }
-//   return true;
+  if (window.innerWidth > 768) {
+    document.getElementById('navigationList').classList.toggle('active');
+    return false;
+  }
+  return true;
 }
-Hamburger.addEventListener('click', menuPopup);
+// Hamburger.addEventListener('click', menuPopup);
+menuPopup();
