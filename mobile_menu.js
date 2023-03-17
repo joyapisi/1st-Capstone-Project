6a846dbc89b181d0cb72a2d520fc00996e55a630
Joy Phoebe <38925103+joyapisi@users.noreply.github.com>
2023-03-17 15:35:20 +0300

    fix es and style linter errors

--- mobile_menu.js
+++ mobile_menu.js
@@ -1,29 +1,10 @@
-// function menuPopup() {
-//   const mobileMenu = document.getElementById("navList");
-//   const CloseButton = document.getElementById('closeButton');
-//   if (mobileMenu.style.display === "block") {
-//     mobileMenu.style.display = "none";
-//   } else {
-//     mobileMenu.style.display = "block";
-//   }
-//   if (CloseButton.style.display === "block") {
-//     CloseButton.style.display = "none";
-//   } else {
-//     CloseButton.style.display = "block";
-//   }
-// }
-
-
-
-
-
 function menuPopup() {
   const NavList = document.getElementById('navList');
-  // const Hamburger = document.getElementById('hamburger');
+  const Hamburger = document.getElementById('hamburger');
   const CloseButton = document.getElementById('closeButton');
 
   NavList.classList.toggle('active');
-  // Hamburger.classList.toggle('active');
+  Hamburger.classList.toggle('active');
   CloseButton.classList.toggle('active');
 
   if (window.innerWidth > 768) {
@@ -33,4 +14,3 @@ function menuPopup() {
   return true;
 }
 Hamburger.addEventListener('click', menuPopup);
-// menuPopup();
